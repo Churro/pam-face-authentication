c6a592f5dba2b6ff300525f8421e0821af14ea8f
armadefuego@gmail.com <armadefuego@gmail.com@23b13dde-1d4e-0410-9555-c5db500eae70>
2013-04-08 15:58:45 +0000

    Save results

--- qtbranch/tests/eyetest.sh
+++ qtbranch/tests/eyetest.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-	time ../eyedf . > eye_results 2>> eye_errors
+DIR=`date "+%Y%m%d%H%M"`
+
+time ../eyedf . > eye_results 2>> eye_errors
+../../eyes.pl eye_results > eye_results.csv 2>> eye_errors
+git diff > diffs
+[ ! -e $DIR ] && mkdir $DIR && mv diffs eye_results eye_errors eye_results.csv $DIR/
